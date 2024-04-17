0387398942a77e9c920cee83e53906107a7ec603
Santi Patiño <santiagopatino@flare.com.co>
2024-04-17 15:34:29 -0500

    Events Onclick and OnChange

--- src/CreateTodoButton.js
+++ src/CreateTodoButton.js
@@ -1,6 +1,12 @@
 function CreateTodoButton() {
     return (
       <button
+      onClick={
+        (event) => {
+          console.log('Lo tocaste');
+         
+        }
+      }
       title="Add New"
       className="group cursor-pointer outline-none hover:rotate-90 duration-300 fixed bottom-[24px] right-[24px] flex justify-center items-center"
     >
@@ -9,14 +15,14 @@ function CreateTodoButton() {
         width="60px"
         height="60px"
         viewBox="0 0 24 24"
-        className="stroke-[#3a31d8] fill-none group-hover:fill-[#010104] group-active:stroke-[#3a31d8] group-active:fill-[#3a31d8] group-active:duration-0 duration-300 "
+        className="stroke-[#3a31d8] fill-none group-hover:fill-[#222248] group-active:stroke-[#3a31d8] group-active:fill-[#222248] group-active:duration-0 duration-300 "
       >
         <path
           d="M12 22C17.5 22 22 17.5 22 12C22 6.5 17.5 2 12 2C6.5 2 2 6.5 2 12C2 17.5 6.5 22 12 22Z"
-          stroke-width="1.5"
+          strokeWidth="1.5"
         ></path>
-        <path d="M8 12H16" stroke-width="1.5"></path>
-        <path d="M12 16V8" stroke-width="1.5"></path>
+        <path d="M8 12H16" strokeWidth="1.5"></path>
+        <path d="M12 16V8" strokeWidth="1.5"></path>
       </svg>
     </button>
     
--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,14 +1,19 @@
 function TodoSearch() {
     return (
-      <div class="flex items-center justify-center ">
-      <div class="relative w-5/6">
+      <div className="flex items-center justify-center ">
+      <div className="relative w-5/6">
         <input
+        onChange={
+          (event) => {
+            console.log('Escribiste en mi');
+            console.log(event.target.value);
+          }}
           type="text"
-          class="w-full border-b border-[#ebe9fc] py-1 focus:border-b-2 focus:border-[#3a31d8] transition-colors focus:outline-none peer bg-inherit"
+          className="w-full border-b border-[#ebe9fc] py-1 focus:border-b-2 focus:border-[#3a31d8] transition-colors focus:outline-none peer bg-inherit"
         />
         <label
           
-          class="absolute left-0 top-1 cursor-text peer-focus:text-xs peer-focus:-top-4 transition-all peer-focus:text-blue-700"
+          className="absolute left-0 top-1 cursor-text peer-focus:text-xs peer-focus:-top-4 transition-all peer-focus:text-blue-700"
           >Search a TODO</label
         >
       </div>
