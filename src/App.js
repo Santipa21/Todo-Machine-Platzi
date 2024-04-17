29f13fcaa0c746606e2d5467e5b145fdea4722cb
Santi Patiño <santiagopatino@flare.com.co>
2024-04-17 16:45:02 -0500

    Searching TODOs

--- src/App.js
+++ src/App.js
@@ -22,6 +22,13 @@ function App() {
   const [searchValue, setSearchValue] = React.useState('');
   const completedTodos = todos.filter(todo => !!todo.completed).length;
   const totalTodos = todos.length;
+  const searchedTodos = todos.filter(
+    (todo) => {
+      const todoText = todo.text.toLowerCase();
+      const searchText = searchValue.toLowerCase();
+      return todoText.includes(searchText);
+    }
+  );
 
   useEffect(() => {
     console.log(searchValue);
@@ -37,7 +44,7 @@ function App() {
       />
 
       <TodoList>
-        {defaultTodos.map(todo => (
+        {searchedTodos.map(todo => (
           <TodoItem
           key={todo.text}
           text={todo.text}
