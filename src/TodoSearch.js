4646c967b34537fb0e01d9389695528119aba1f9
Santi Patiño <santiagopatino@flare.com.co>
2024-04-17 16:05:45 -0500

    What is useState?

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,18 +1,25 @@
+import React from "react";
+import { useEffect } from "react";
+
 function TodoSearch() {
+  const [searchValue, setSearchValue] = React.useState('');
+
+  useEffect(() => {
+    console.log(searchValue);
+  }, [searchValue])
     return (
       <div className="flex items-center justify-center ">
       <div className="relative w-5/6">
         <input
+        value={searchValue}
         onChange={
           (event) => {
-            console.log('Escribiste en mi');
-            console.log(event.target.value);
+            setSearchValue(event.target.value)
           }}
           type="text"
           className="w-full border-b border-[#ebe9fc] py-1 focus:border-b-2 focus:border-[#3a31d8] transition-colors focus:outline-none peer bg-inherit"
         />
         <label
-          
           className="absolute left-0 top-1 cursor-text peer-focus:text-xs peer-focus:-top-4 transition-all peer-focus:text-blue-700"
           >Search a TODO</label
         >
