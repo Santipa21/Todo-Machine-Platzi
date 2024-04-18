5b51821e63db75c5d77056c78fa8134471b07456
Santi Patiño <santiagopatino@flare.com.co>
2024-04-18 14:45:40 -0500

    React Context

--- src/App/index.js
+++ src/App/index.js
@@ -1,72 +1,13 @@
 import React from "react";
-import { useEffect } from "react";
-import { useLocalStorage } from './useLocalStorage';
+import { TodoProvider } from "../TodoContext";
 import { AppUI } from './AppUi';
 
-// localStorage.removeItem('TODOS_V1');
-
-// const defaultTodos = [
-//   {text: 'Desayunar', completed: true},
-//   {text: 'Ir al Gym', completed: false},
-//   {text: 'Trabajar', completed: true},
-//   {text: 'Estudiar', completed: false},
-//   {text: 'Almorzar', completed: true},
-//   {text: 'Dormir', completed: false},
-// ];
-// localStorage.setItem('TODOS_V1', JSON.stringify(defaultTodos));
-
 function App() {
-  const {
-    item:todos, 
-    saveItem:saveTodos,
-    loading,
-    error
-  } = useLocalStorage('TODOS_V1', []);
-  const [searchValue, setSearchValue] = React.useState('');
-  const completedTodos = todos.filter(todo => !!todo.completed).length;
-  const totalTodos = todos.length;
-  const searchedTodos = todos.filter(
-    (todo) => {
-      const todoText = todo.text.toLowerCase();
-      const searchText = searchValue.toLowerCase();
-      return todoText.includes(searchText);
-    }
-  );
-
-  useEffect(() => {
-    console.log(searchValue);
-  }, [searchValue]);
-
-  const completeTodo = (text) => {
-    const newTodos = [...todos];
-    const todoIndex =newTodos.findIndex(
-      (todo) => todo.text === text
-    );
-    newTodos[todoIndex].completed = true;
-    saveTodos(newTodos);
-  };
-
-  const deleteTodo = (text) => {
-    const newTodos = [...todos];
-    const todoIndex =newTodos.findIndex(
-      (todo) => todo.text === text
-    );
-    newTodos.splice(todoIndex, 1);
-    saveTodos(newTodos);
-  };
 
   return(
-    <AppUI
-    completedTodos={completedTodos}
-    totalTodos={totalTodos}
-    searchValue={searchValue}
-    setSearchValue={setSearchValue}
-    searchedTodos={searchedTodos}
-    completeTodo={completeTodo}
-    deleteTodo={deleteTodo}
-    loading={loading}
-    error={error}
-    />
+   <TodoProvider>
+     <AppUI/>
+   </TodoProvider>
   );
 }
 
