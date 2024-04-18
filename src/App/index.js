3fd2eb5b1594226d9ae96f351dd8425a35780d29
Santi Patiño <santiagopatino@flare.com.co>
2024-04-18 11:14:28 -0500

    Organitation of files and folders and directions in React

--- src/App/index.js
+++ src/App/index.js
@@ -1,10 +1,11 @@
-import { TodoCounter } from './TodoCounter';
-import { TodoSearch } from './TodoSearch';
-import { TodoList } from './TodoList';
-import { CreateTodoButton } from './CreateTodoButton';
-import { TodoItem } from './TodoItem';
+import { TodoCounter } from '../TodoCounter';
+import { TodoSearch } from '../TodoSearch';
+import { TodoList } from '../TodoList';
+import { CreateTodoButton } from '../CreateTodoButton';
+import { TodoItem } from '../TodoItem';
 import React from "react";
 import { useEffect } from "react";
+import { useLocalStorage } from './useLocalStorage';
 
 // const defaultTodos = [
 //   {text: 'Desayunar', completed: true},
@@ -16,26 +17,6 @@ import { useEffect } from "react";
 // ];
 // localStorage.setItem('TODOS_V1', JSON.stringify(defaultTodos));
 // localStorage.removeItem('TODOS_V1');
-
-function useLocalStorage(itemName, initialValue) {
-  const localStorageItem = localStorage.getItem(itemName);
-  let parsedItem;
-
-  if (!localStorageItem) {
-    localStorage.setItem(itemName, JSON.stringify(initialValue));
-    parsedItem = initialValue;
-  } else {
-    parsedItem = JSON.parse(localStorageItem);
-  }
-  const [item, setItem] = React.useState(parsedItem);
-
-  const saveItem = (newItem) => {
-    localStorage.setItem(itemName, JSON.stringify(newItem));
-    setItem(newItem);
-  }
-
-  return [item, saveItem];
-}
 
 function App() {
   const [todos, saveTodos] = useLocalStorage('TODOS_V1', []);
